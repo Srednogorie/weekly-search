4c7f332be0708f2af0b26ce53c7cb41692f5cc40
Leno <akrachunov@gmail.com>
2022-09-10 18:43:05 +0100

    Hide reset button.

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,11 +36,11 @@ const Home: NextPage = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
                                 </svg>
                       }
-                      resetIconComponent={
-                          () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#264653]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                      }
+                      // resetIconComponent={
+                      //     () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#264653]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      //               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      //           </svg>
+                      // }
                   />
                   <div className={"min-h-[1.5rem] mb-4"}>
                       <CustomRefinementList attribute="provider"/>
