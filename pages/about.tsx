2c4eff73aa645880ce7af896ac2d5193ac8ff9d6
Leno <akrachunov@gmail.com>
2022-10-01 11:30:22 +0300

    Fix DOM error.

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,14 +8,14 @@ const About: NextPage = () => {
         <div className="flex flex-col !h-screen justify-between">
             <NavBar/>
             <div className="flex flex-col items-center mb-auto mx-4">
-                <p className="italic text-[#F4A261] text-center my-6">
+                <div className="italic text-[#F4A261] text-center my-6">
                     <div className={"flex items-center"}>
                         <Image src="/octocat.jpg" alt="Picture of the author" width={30} height={30}/>
                         <a href="https://github.com/Srednogorie/weekly-search" target="_blank" rel="noreferrer" className={"underline text-[#E76F51] ml-2"}>
                             github.com/Srednogorie/weekly-search
                         </a>
                     </div>
-                </p>
+                </div>
                 <h1 className="text-[#264653] text-2xl py-3">Frontend</h1>
                 <p className="italic text-[#F4A261] text-center">React, NextJs, Typescript, Tailwind, SWR, Formik, Algolia&apos;s react-instantsearch-hooks</p>
                 <p className="italic text-[#F4A261] text-center">Color scheme - <a href="https://coolors.co/palette/264653-2a9d8f-e9c46a-f4a261-e76f51" target={"_blank"} rel={"noreferrer"} className={"underline text-[#E76F51]"}>coolors.co</a></p>
