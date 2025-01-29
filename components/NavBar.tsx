102eed2006218b5231cbe6ee2516fccb503af52b
Leno <akrachunov@gmail.com>
2025-01-29 09:08:03 +0200

    fix Link component, use legacyBehavior.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,21 @@
-import React from "react"
 import Image from "next/image"
 import Link from "next/link"
+import React from "react"
 
 const NavBar: React.FC = () => {
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded mb-6">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <div className="relative w-40 h-10 cursor-pointer">
-                    <Link href="/" className="flex items-center">
+                    {/* Link with <a> tag isn't allowed, use legacyBehavior or fix*/}
+                    <Link href="/" className="flex items-center" legacyBehavior>
                         <a>
                             <Image src="/JsSearch.png" className="mr-3 h-6 sm:h-9" alt="JsSearch Logo" layout="fill"/>
                         </a>
                     </Link>
                 </div>
                 <div className="flex mt-4">
-                    <Link href="/bookmarks">
+                    <Link href="/bookmarks" legacyBehavior>
                         <a className={"cursor-pointer has-tooltip"}>
                             <span className='tooltip rounded shadow-lg p-1 bg-[#264653] text-white mt-8 opacity-70 text-xs'>Bookmarks</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="#264653">
@@ -22,7 +23,7 @@ const NavBar: React.FC = () => {
                             </svg>
                         </a>
                     </Link>
-                    <Link href="/about">
+                    <Link href="/about" legacyBehavior>
                         <a className={"cursor-pointer has-tooltip ml-5"}>
                             <span className='tooltip rounded shadow-lg p-1 bg-[#264653] text-white mt-8 opacity-70 text-xs'>About</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="#264653" strokeWidth={2}>
